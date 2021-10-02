4010f84f4a907c8ff396491e8352a25f0f433493
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-10-02 17:08:38 +1300

    Add migrations on startup

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace FizzBuzz
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDBContext context)
         {
             if (env.IsDevelopment())
             {
@@ -45,6 +45,10 @@ namespace FizzBuzz
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Ensure database is updated with all migrations on startup
+            context.Database.Migrate();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 

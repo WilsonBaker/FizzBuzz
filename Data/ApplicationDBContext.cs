548c9a3db855146c6c6f9bf4ce5c4f75f3b88fc3
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-10-02 15:08:26 +1300

    Add result model to dbcontext

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using FizzBuzz.Models;
 
 namespace FizzBuzz.Data
 {
@@ -12,5 +13,7 @@ namespace FizzBuzz.Data
         {
 
         }
+
+        public DbSet<Result> Results { get; set; }
     }
 }

8e97b3b9a0a8bd3ac959771e57a209088f4d8d61
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-10-02 14:58:00 +1300

    Add constructor to Db

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -8,5 +8,9 @@ namespace FizzBuzz.Data
 {
     public class ApplicationDBContext: DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
     }
 }

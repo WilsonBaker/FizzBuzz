c46498c5373b35e1de5963c3239af9d3062014ef
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-10-03 14:12:44 +1300

    Add FizzBuzz seed data to migration

--- Migrations/20211003010951_SeedData.Designer.cs
+++ Migrations/20211003010951_SeedData.Designer.cs
@@ -3,14 +3,16 @@ using FizzBuzz.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FizzBuzz.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211003010951_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

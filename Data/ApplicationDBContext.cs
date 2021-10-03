a0022ea4788aebea174ed488fbfaab34c910318d
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-10-03 13:43:03 +1300

    Override OnModelCreating
    Adds all configurations in the project. In this case we want the seed data

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using FizzBuzz.Models;
+using System.Reflection;
 
 namespace FizzBuzz.Data
 {
@@ -15,5 +16,11 @@ namespace FizzBuzz.Data
         }
 
         public DbSet<Result> Results { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Add 
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
